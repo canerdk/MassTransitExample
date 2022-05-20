1d4eebbc7b048e04ba196d8c80d6d3454bd40fed
Caner <caner.demirkaya@secilstore.com>
2022-05-20 14:15:23 +0300

    created rabbitmq extension

---
+++ NetCoreMassTransit/BuildingBlocks/EventBus.Messages/Events/OrderStatusResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public interface OrderStatusResult
+    {
+        string OrderId { get; }
+        DateTime Timestamp { get; }
+        short StatusCode { get; }
+        string StatusText { get; }
+    }
+}
---
+++ NetCoreMassTransit/BuildingBlocks/EventBus.Messages/Extensions/RabbitMqExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Extensions
+{
+    public static class RabbitMqExtension
+    {
+        public static IServiceCollection AddMasstransitWithRabbitMQ(this IServiceCollection services)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger<IApplicationBuilder>();
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+            services.AddMassTransit(x => {
+                x.AddConsumers(Assembly.GetEntryAssembly());
+                x.UsingRabbitMq((ctx, cfg) => {
+                    cfg.Host(configuration["EventBusSettings:HostAddress"], host =>
+                    {
+                        host.Username("guest");
+                        host.Password("guest");
+                    });
+                    cfg.ConfigureEndpoints(ctx, new KebabCaseEndpointNameFormatter(configuration["ServiceName"], false));
+                });
+            });
+
+            services.AddGenericRequestClient();
+
+            services.AddMassTransitHostedService(true);
+
+            return services;
+        }
+    }
+}
